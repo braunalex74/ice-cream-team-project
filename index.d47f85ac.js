d2bf86a6f7ff3e2b60a809a5dffae8ced9d6028e
VlasiukOleg <106608565+VlasiukOleg@users.noreply.github.com>
2023-01-11 15:16:51 +0000

    Deploying to gh-pages from @ VlasiukOleg/ice-cream-team-project@ab415de8cac3df3dc5e8eb8f7ae42607f5c287d0 🚀

---
+++ index.d47f85ac.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelectorAll("#slides .slide"),l=0;setInterval((function(){e[l].className="slide",l=(l+1)%e.length,e[l].className="slide showing"}),5e3)}();
+//# sourceMappingURL=index.d47f85ac.js.map
---
+++ index.e644b87c.js
@@ -0,0 +1,2 @@
+const e=document.querySelectorAll("#slides .slide");let l=0;setInterval((function(){e[l].className="slide",l=(l+1)%e.length,e[l].className="slide showing"}),5e3);
+//# sourceMappingURL=index.e644b87c.js.map
